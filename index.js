2a376900f899b25988f896bb1d6d08ed4b132b76
Robert Boghian <robert.boghian@beyondenterprizes.com>
2022-08-25 08:25:20 +0300

    example app + added the route name in device info

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/**
- * @format
- */
+import Shkr from "./src/Shkr";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+export { Shkr };
